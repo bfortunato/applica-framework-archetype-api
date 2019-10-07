e4f1efdc70c1c8e4145b64403947cdbda1b93766
bimbobruno <bimbobruno@gmail.com>
2019-10-07 12:18:21 +0200

    Renamed packages

---
+++ api-runner/src/main/java/applica/api/runner/operations/UserGetOperation.java
@@ -0,0 +1,28 @@
+package applica.api.runner.operations;
+
+import applica.api.domain.model.User;
+import applica.framework.Entity;
+import applica.framework.widgets.operations.BaseGetOperation;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by bimbobruno on 24/01/2017.
+ */
+
+@Component
+public class UserGetOperation extends BaseGetOperation {
+
+    @Override
+    protected void finishNode(Entity entity, ObjectNode node) {
+        map().imageToDataUrl(entity, node, "image", "_image", "150x*");
+        map().imageToDataUrl(entity, node, "coverImage", "_cover", "150x*");
+    }
+
+    @Override
+    public Class<? extends Entity> getEntityType() {
+        return User.class;
+    }
+
+
+}
---
+++ api-runner/src/main/java/applica/api/runner/permissions/Permissions.java
@@ -0,0 +1,25 @@
+package applica.api.runner.permissions;
+
+
+
+import applica.api.domain.model.Role;
+
+import java.util.*;
+
+public class Permissions {
+    public static final String ADMIN = "permission:admin";
+
+    public static List<String> getPermissionByRole(String role) {
+        switch (role) {
+            case Role.ADMIN:
+                return Arrays.asList(getAllPermissions());
+        }
+        return new ArrayList<>();
+    }
+
+    public static String[] getAllPermissions(){
+        return new String[]{ADMIN};
+    }
+}
+
+
